259832d32f835adc02b1a92685f3f8458d2081d1
Arshlan khan <arshlankhan69@Arshlans-MacBook-Air.local>
2026-07-24 03:02:13 +0530

    make plant title dynamic

--- backend/init/index.js
+++ backend/init/index.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const mongoose = require("mongoose")
 const initdata = require("./data.js")
 
 const Listing = require("../models/listing.js")
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/herbal_garden"
+const MONGO_URL = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/herbal_garden"
 
 main().then(() => { console.log("Connected to DB") }).catch((err) => {
     console.log(err)
@@ -12,11 +13,10 @@ async function main() {
     mongoose.connect(MONGO_URL);
 }
 
-const initDB =async () =>{
-   await Listing.deleteMany({})
+const initDB = async () => {
+    await Listing.deleteMany({})
     await Listing.insertMany(initdata.data)
-     console.log("data was initialized")
+    console.log("data was initialized")
 }
 
 initDB();
-
--- backend/init/index3.js
+++ backend/init/index3.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const mongoose = require("mongoose")
 const initdata = require("./infodata.js")
 
 const Information = require("../models/listing3.js")
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/herbal_garden"
+const MONGO_URL = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/herbal_garden"
 
 main().then(() => { console.log("Connected to DB") }).catch((err) => {
     console.log(err)
@@ -12,11 +13,11 @@ async function main() {
     mongoose.connect(MONGO_URL);
 }
 
-const initDB =async () =>{
-        await Information.deleteMany({})
-        await Information.insertMany(initdata)
+const initDB = async () => {
+    await Information.deleteMany({})
+    await Information.insertMany(initdata)
 
-     console.log("data was initialized")
+    console.log("data was initialized")
 }
 
 initDB();
